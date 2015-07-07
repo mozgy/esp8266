3e5c6e79dd2eb9434a08e3c7c8b5c1e4c08ed127
Mario Mikočević <mozgy@t-com.hr>
2015-07-07 19:27:31 +0200

    typos

--- OLED_SSD1306/OLED_SSD1306.cpp
+++ OLED_SSD1306/OLED_SSD1306.cpp
@@ -1,17 +1,19 @@
 /*
-  OLED_SSD1306.cpp - I2C 128x64 OLED Driver Library
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-*/
+ *   OLED_SSD1306.cpp - I2C 128x64 OLED Driver Library
+ *     
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   Copyright (c) 2015. Mario Mikočević <mozgy>
+ *
+ */
 
 #include <Wire.h>
 // #include <avr/pgmspace.h>
